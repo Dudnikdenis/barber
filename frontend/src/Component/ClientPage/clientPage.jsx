e906f803cd341af3e88a16990973c02d061f5080
Dudnikdenis <dudnik.denis.87@mail.ru>
2023-08-01 18:02:53 +0500

    href

--- frontend/src/Component/ClientPage/clientPage.jsx
+++ frontend/src/Component/ClientPage/clientPage.jsx
@@ -17,7 +17,10 @@ const ClientPage = () => {
                 </span> 
                 <span className={cs.divLink}>
                     <a className={cs.link} href="http://104.154.140.254:8080/swagger-ui/index.html" >Swagger </a>
-                </span>               
+                </span>  
+                <span className={cs.divLink}>
+                    <a className={cs.link} href="https://www.youtube.com/watch?v=yYq0rWESsNY&t=147s" >полезная ссылка </a>
+                </span>              
             </header>            
         </div>
     )
